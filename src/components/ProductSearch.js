25f3c548af5005ebeb13ae8c6f2d414ce4904ea8
justyasen <ymanahov@gmail.com>
2022-01-05 12:10:53 +0200

    html file added

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -3,7 +3,11 @@ import React from "react";
 class ProductSearch extends React.Component{
     render() {
         return(
-            <p>Product Search works! </p>
+            <form className="product-search"> 
+                <h2> Enter a product </h2> 
+                <input type= "submit" required placeholder="product" />
+                <button type="submit"> Search 🔍 </button>   
+            </form>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 //Importing components for reusability
 import ProductSearch from './components/ProductSearch'; 
 import ProductCategory from './components/ProductCategory'; 
@@ -11,15 +9,6 @@ import Comments from './components/Comments';
 import Ratings from './components/Ratings'; 
 import Contacts from './components/Contacts'; 
 import Help from './components/Help'; 
+import { render } from '@testing-library/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<ProductSearch />, document.querySelector('#main'))
